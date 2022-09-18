e62c3eacad30dc3ca0efd5e5163715012ee6239b
DoCi-Mk <80265901+DoCi-Mk@users.noreply.github.com>
2022-09-19 02:13:09 +0430

    update ready

--- events/ready.js
+++ events/ready.js
@@ -6,8 +6,28 @@ const config = require('../config.json')
 
 client.on('ready', async () => {
 
+  let targetGuild = client.guilds.cache.get("933722860856430722");
+
+  //Update Channel
+  setInterval(function () {
+    client.channels.fetch("1021141488693948446").then((channel) => {
+      channel.edit({
+        name: `👥・${targetGuild.memberCount} Members`,
+      });
+    });
+
+    client.channels.fetch("1021172293990350858").then((channel) => {
+      channel.edit({
+        name: `🪐・${client.guilds.cache.size} Guild's`,
+      });
+    });
+  }, 1 * 60 * 1000);
+
+  //Set Presence
   client.user.setPresence({ status: "idle" });
   global.servers = client.guilds.cache.size
+
+  //Logged in & Activity
   console.log(red(`${client.user.tag} Ready!\nActive on ${client.guilds.cache.size} guild's!`));
   client.user.setActivity(`${config.version} | /help`, { type: "WATCHING" })
 
