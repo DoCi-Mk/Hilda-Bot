8440bfb0778d332bad10ba3ac4e6bcb6a2726158
DoCi-Mk <80265901+DoCi-Mk@users.noreply.github.com>
2022-09-18 23:17:00 +0430

    slashCommad fixed

--- SlashCommands/mod/hi.js
+++
@@ -1,8 +0,0 @@
-const { MessageEmbed } = require("discord.js")
-module.exports = {
-        name : "hi",
-        description : "فاک یو",
-        run : async (client, interaction, args) => {
-             interaction.editReply("Hi")
-       }
-}
